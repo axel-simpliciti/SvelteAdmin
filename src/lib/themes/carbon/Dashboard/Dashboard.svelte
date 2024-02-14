d4bdd3fb99432a0b4125f989ed8316c4c3a31f07
avallier <axel.vallier@simpliciti.fr>
2024-02-14 16:57:31 +0100

    WIP

--- src/lib/themes/carbon/Dashboard/Dashboard.svelte
+++ src/lib/themes/carbon/Dashboard/Dashboard.svelte
@@ -41,13 +41,20 @@
 		currentCrudOperation?.dashboard.adminConfig.theme.crudActions[
 			currentCrudOperation?.displayComponentName
 		];
-	const sideMenu = dashboard.stores.sideMenuLinks
+
+	let value;
+	dashboard.stores.subscribe((val) => {
+		value = val.sideMenu
+	})
+
+
+
 </script>
 
 <AdminLayout
 	adminConfig={dashboard.adminConfig}
 	translations={dashboard.localeDictionaries}
-	side_menu_links={sideMenu}
+	side_menu_links={value}
 	top_left_menu_links={dashboard.topLeftMenu}
 	top_right_menu_links={dashboard.topRightMenu}
 >
